ffd5ac1595aa6b648b66c54c4018de38215f27e2
dhelle <rachel4803@gmail.com>
2022-09-11 23:54:22 +0900

    library backend settings

---
+++ part8.graphql/library_be/src/index.js
@@ -0,0 +1,9 @@
+const { ApolloServer, gql } = require("apollo-server");
+import { typeDefs } from "./typeDefs/index.js";
+import { resolvers } from "./resolvers/index.js";
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
